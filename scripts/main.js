49d5923f4ade6bc56c4044a45026f82cb3461c46
Nichole Godwin <headiehg@mail.com>
2020-08-02 23:16:25 -0700

    progress

--- scripts/main.js
+++ scripts/main.js
@@ -7,85 +7,88 @@ var options = {
 };
 
 function success(pos) {
-  console.log('pos',pos);
+  console.log('pos', pos);
   var crd = pos.coords;
   console.log(crd);
   var sunapi = `https://api.sunrise-sunset.org/json?lat=${crd.latitude}&lng=${crd.longitude}&date=today`;
   console.log('url:', sunapi);
   request.open('GET', sunapi, true)
-  request.onload = function () {
+  request.onload = function() {
     var data = JSON.parse(this.response)
     if (request.status >= 400 || request.status < 200) {
-      var data = {"results":{"sunrise":"1:08:18 PM","sunset":"3:13:20 AM","solar_noon":"8:10:49 PM","day_length":"14:05:02","civil_twilight_begin":"12:39:02 PM","civil_twilight_end":"3:42:35 AM","nautical_twilight_begin":"12:03:12 PM","nautical_twilight_end":"4:18:25 AM","astronomical_twilight_begin":"11:24:22 AM","astronomical_twilight_end":"4:57:15 AM"},"status":"OK"}
+      var data = {
+        "results": {
+          "sunrise": "1:08:18 PM",
+          "sunset": "3:13:20 AM",
+          "solar_noon": "8:10:49 PM",
+          "day_length": "14:05:02",
+          "civil_twilight_begin": "12:39:02 PM",
+          "civil_twilight_end": "3:42:35 AM",
+          "nautical_twilight_begin": "12:03:12 PM",
+          "nautical_twilight_end": "4:18:25 AM",
+          "astronomical_twilight_begin": "11:24:22 AM",
+          "astronomical_twilight_end": "4:57:15 AM"
+        },
+        "status": "OK"
+      }
     }
-      var dateLocal = new Date();
-      var hr = dateLocal.getUTCHours()*100;
-      var mins =  dateLocal.getUTCMinutes();
-      var timeA = hr+mins
-      var timeS = data.results.sunset;
-      var timeR = data.results.sunrise;
-      function timeAlter(time) {
-        var hours = Number(time.match(/^(\d+)/)[1]);
-        var minutes = Number(time.match(/:(\d+)/)[1]);
-        var meridiem = time.match(/\s(.*)$/)[1];
-        if(meridiem == "PM" && hours<12){
-          hours = hours+12;
-        }
-        if(meridiem == "AM" && hours==12) {
-          hours = hours-12;}
-        var timeV = (hours*100)+minutes
-        return timeV;
+    var dateLocal = new Date();
+    var hr = dateLocal.getUTCHours() * 100;
+    var mins = dateLocal.getUTCMinutes();
+    var timeA = hr + mins
+    var timeS = data.results.sunset;
+    var timeR = data.results.sunrise;
+
+    function timeAlter(time) {
+      var hours = Number(time.match(/^(\d+)/)[1]);
+      var minutes = Number(time.match(/:(\d+)/)[1]);
+      var meridiem = time.match(/\s(.*)$/)[1];
+      if (meridiem == "PM" && hours < 12) {
+        hours = hours + 12;
       }
-
-      console.log('sunset',timeAlter(timeS));
-      console.log('sunrise',timeAlter(timeR));
-      console.log('actual time',timeA);
-      let nothingMakesSense = timeAlter(timeS) < timeAlter(timeR); //because of UTC timezone
-      let isdaytime = true;
-      if((timeAlter(timeS) < timeA && timeA <= 2400) || (0 <= timeA && timeA <  timeAlter(timeR))) {
-        isdaytime=false;
+      if (meridiem == "AM" && hours == 12) {
+        hours = hours - 12;
       }
-      if(nothingMakesSense){
-        isdaytime = !isdaytime;
-      }
-      console.log('final',isdaytime)
+      var timeV = (hours * 100) + minutes
+      return timeV;
+    }
 
-      var item = document.getElementById("item");
-      console.log(item);
+    console.log('sunset', timeAlter(timeS));
+    console.log('sunrise', timeAlter(timeR));
+    console.log('actual time', timeA);
+    let nothingMakesSense = timeAlter(timeS) < timeAlter(timeR); //because of UTC timezone
+    let isdaytime = true;
+    //if ((timeAlter(timeS) < timeA && timeA <= 2400) || (0 <= timeA && timeA < timeAlter(timeR))) {
 
-      var iImg = document.createElement("img");
-      var bgImg = document.getElementById("backgroundimage");
-      console.log('bg', bgImg);
+    //312 < 607 & 607 < 1309
+    if ((timeAlter(timeS) < timeA && timeA < timeAlter(timeR))) {
+      isdaytime = true;
+      console.log('day')
+    }
+    if (nothingMakesSense) {
+      isdaytime = !isdaytime;
+      console.log('backwards')
+    }
 
-      if(isdaytime){
-        iImg.setAttribute('src', 'img/balloon.png');
-        iImg.setAttribute('alt', 'floating red baloon with long red string attached to it');
-        bgImg.style.backgroundImage = "url('../img/clouds.png')";      }
-      else{
-        iImg.setAttribute('src', 'img/astronaut.png');
-        iImg.setAttribute('alt', 'floating astronaut');
-        bgImg.style.backgroundImage = "url('../img/star.png')";
-      }
-      item.appendChild(iImg);
+    console.log('final', isdaytime)
 
+    var item = document.getElementById("item");
+    console.log(item);
 
+    var iImg = document.createElement("img");
+    var bgImg = document.getElementById("backgroundimage");
+    console.log('bg', bgImg);
 
-
-      /*data.forEach((movie) => {
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card')
-
-        const h1 = document.createElement('h1')
-        h1.textContent = movie.title
-
-        const p = document.createElement('p')
-        movie.description = movie.description.substring(0, 300)
-        p.textContent = `${movie.description}...`
-
-        container.appendChild(card)
-        card.appendChild(h1)
-        card.appendChild(p)
-      }*/
+    if (isdaytime) {
+      iImg.setAttribute('src', 'img/balloon.png');
+      iImg.setAttribute('alt', 'floating red baloon with long red string attached to it');
+      bgImg.style.backgroundImage = "url('../img/clouds.png')";
+    } else {
+      iImg.setAttribute('src', 'img/astronaut.png');
+      iImg.setAttribute('alt', 'floating astronaut');
+      bgImg.style.backgroundImage = "url('../img/star.png')";
+    }
+    item.appendChild(iImg);
 
   }
   request.send()
@@ -97,63 +100,79 @@ function error(err) {
   var sunapi = 'https://api.sunrise-sunset.org/json?lat=38.6367488&lng=-121.15968000000001&date=today';
   console.log('url:', sunapi);
   request.open('GET', sunapi, true)
-  request.onload = function () {
+  request.onload = function() {
     var data = JSON.parse(this.response)
     if (request.status >= 400 || request.status < 200) {
-      var data = {"results":{"sunrise":"1:08:18 PM","sunset":"3:13:20 AM","solar_noon":"8:10:49 PM","day_length":"14:05:02","civil_twilight_begin":"12:39:02 PM","civil_twilight_end":"3:42:35 AM","nautical_twilight_begin":"12:03:12 PM","nautical_twilight_end":"4:18:25 AM","astronomical_twilight_begin":"11:24:22 AM","astronomical_twilight_end":"4:57:15 AM"},"status":"OK"}
+      var data = {
+        "results": {
+          "sunrise": "1:08:18 PM",
+          "sunset": "3:13:20 AM",
+          "solar_noon": "8:10:49 PM",
+          "day_length": "14:05:02",
+          "civil_twilight_begin": "12:39:02 PM",
+          "civil_twilight_end": "3:42:35 AM",
+          "nautical_twilight_begin": "12:03:12 PM",
+          "nautical_twilight_end": "4:18:25 AM",
+          "astronomical_twilight_begin": "11:24:22 AM",
+          "astronomical_twilight_end": "4:57:15 AM"
+        },
+        "status": "OK"
+      }
     }
-      var dateLocal = new Date();
-      var hr = dateLocal.getUTCHours()*100;
-      var mins =  dateLocal.getUTCMinutes();
-      var timeA = hr+mins
-      var timeS = data.results.sunset;
-      var timeR = data.results.sunrise;
-      function timeAlter(time) {
-        var hours = Number(time.match(/^(\d+)/)[1]);
-        var minutes = Number(time.match(/:(\d+)/)[1]);
-        var meridiem = time.match(/\s(.*)$/)[1];
-        if(meridiem == "PM" && hours<12){
-          hours = hours+12;
-        }
-        if(meridiem == "AM" && hours==12) {
-          hours = hours-12;}
-        var timeV = (hours*100)+minutes
-        return timeV;
-      }
+    var dateLocal = new Date();
+    var hr = dateLocal.getUTCHours() * 100;
+    var mins = dateLocal.getUTCMinutes();
+    var timeA = hr + mins
+    var timeS = data.results.sunset;
+    var timeR = data.results.sunrise;
 
-      console.log('sunset',timeAlter(timeS));
-      console.log('sunrise',timeAlter(timeR));
-      console.log('actual time',timeA);
-      let nothingMakesSense = timeAlter(timeS) < timeAlter(timeR); //because of UTC timezone
-      let isdaytime = true;
-      if((timeAlter(timeS) < timeA && timeA <= 2400) || (0 <= timeA && timeA <  timeAlter(timeR))) {
-        isdaytime=false;
+    function timeAlter(time) {
+      var hours = Number(time.match(/^(\d+)/)[1]);
+      var minutes = Number(time.match(/:(\d+)/)[1]);
+      var meridiem = time.match(/\s(.*)$/)[1];
+      if (meridiem == "PM" && hours < 12) {
+        hours = hours + 12;
       }
-      if(nothingMakesSense){
-        isdaytime = !isdaytime;
+      if (meridiem == "AM" && hours == 12) {
+        hours = hours - 12;
       }
-      console.log('final',isdaytime)
+      var timeV = (hours * 100) + minutes
+      return timeV;
+    }
 
-      var item = document.getElementById("item");
-      console.log(item);
+    console.log('sunset', timeAlter(timeS));
+    console.log('sunrise', timeAlter(timeR));
+    console.log('actual time', timeA);
+    let nothingMakesSense = timeAlter(timeS) < timeAlter(timeR); //because of UTC timezone
+    let isdaytime = true;
+    if ((timeAlter(timeS) < timeA && timeA <= 2400) || (0 <= timeA && timeA < timeAlter(timeR))) {
+      isdaytime = false;
+    }
+    if (nothingMakesSense) {
+      isdaytime = !isdaytime;
+    }
+    console.log('final', isdaytime)
 
-      var iImg = document.createElement("img");
-      var bgImg = document.getElementById("backgroundimage");
-      console.log('bg', bgImg);
+    var item = document.getElementById("item");
+    console.log(item);
 
-      if(isdaytime){
-        iImg.setAttribute('src', 'img/balloon.png');
-        iImg.setAttribute('alt', 'floating red baloon with long red string attached to it');
-        bgImg.style.backgroundImage = 'url("img/clouds.png")';
-        bgImg.style.color = "black";
-       }//../img/clouds.png
-      else{
-        iImg.setAttribute('src', 'img/astronaut.png');
-        iImg.setAttribute('alt', 'floating astronaut');
-        bgImg.style.backgroundImage = 'url("img/star.png")';//../img/star.png
-        bgImg.style.color = "white";
-      }
-      item.appendChild(iImg);
+    var iImg = document.createElement("img");
+    var bgImg = document.getElementById("backgroundimage");
+    console.log('bg', bgImg);
+
+    if (isdaytime) {
+      iImg.setAttribute('src', 'img/balloon.png');
+      iImg.setAttribute('alt', 'floating red baloon with long red string attached to it');
+      bgImg.style.backgroundImage = 'url("img/clouds.png")';
+      bgImg.style.color = "black";
+    } //../img/clouds.png
+    else {
+      iImg.setAttribute('src', 'img/astronaut.png');
+      iImg.setAttribute('alt', 'floating astronaut');
+      bgImg.style.backgroundImage = 'url("img/star.png")'; //../img/star.png
+      bgImg.style.color = "white";
+    }
+    item.appendChild(iImg);
   }
   request.send()
 
@@ -162,8 +181,8 @@ function error(err) {
 
 navigator.geolocation.getCurrentPosition(success, error, options);
 
-    //var position.latitude = 51.5;
-    //var position.longitude = -0.1;
+//var position.latitude = 51.5;
+//var position.longitude = -0.1;
 
-    //var latitude = 38.6;
-    //var longitude = 121.1;
+//var latitude = 38.6;
+//var longitude = 121.1;
