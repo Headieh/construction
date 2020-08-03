0c657485eb723447cddd8a6455e14d06323d8028
Nichole Godwin <headiehg@mail.com>
2020-08-03 14:25:53 -0700

    restart

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,9 @@ var options = {
 };
 
 function success(pos) {
-  console.log('pos', pos);
   var crd = pos.coords;
-  console.log(crd);
   var sunapi = `https://api.sunrise-sunset.org/json?lat=${crd.latitude}&lng=${crd.longitude}&date=today`;
+  console.log('location was given and working');
   console.log('url:', sunapi);
   request.open('GET', sunapi, true)
   request.onload = function() {
@@ -58,9 +57,7 @@ function success(pos) {
     console.log('actual time', timeA);
     let nothingMakesSense = timeAlter(timeS) < timeAlter(timeR); //because of UTC timezone
     let isdaytime = false;
-    //if ((timeAlter(timeS) < timeA && timeA <= 2400) || (0 <= timeA && timeA < timeAlter(timeR))) {
 
-    //312 < 607 & 607 < 1309
     if ((timeAlter(timeS) < timeA && timeA < timeAlter(timeR))) {
       isdaytime = true;
       console.log('day')
@@ -69,20 +66,16 @@ function success(pos) {
       isdaytime = !isdaytime;
       console.log('backwards')
     }
-
-    console.log('final', isdaytime)
+    console.log('daytime:', isdaytime)
 
     var item = document.getElementById("item");
-    console.log(item);
-
     var iImg = document.createElement("img");
     var bgImg = document.getElementById("backgroundimage");
-    console.log('bg', bgImg);
-    console.log('not the error');
 
-    if (isdaytime) {
+    if (isdaytime) { //../img/clouds.png
       iImg.setAttribute('src', 'img/balloon.png');
       iImg.setAttribute('alt', 'floating red baloon with long red string attached to it');
+      iImg.className = 'img-fluid'
       bgImg.style.backgroundImage = "url('https://raw.githubusercontent.com/Headieh/construction/master/img/clouds.png')";
     } else {
       iImg.setAttribute('src', 'img/astronaut.png');
@@ -90,16 +83,17 @@ function success(pos) {
       bgImg.style.backgroundImage = "url('https://raw.githubusercontent.com/Headieh/construction/master/img/star.png')";
     }
     item.appendChild(iImg);
-
   }
   request.send()
 }
 
 function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
+  console.log('location was not given and defualt CA sunset sunrise times were used');
 
   var sunapi = 'https://api.sunrise-sunset.org/json?lat=38.6367488&lng=-121.15968000000001&date=today';
   console.log('url:', sunapi);
+
   request.open('GET', sunapi, true)
   request.onload = function() {
     var data = JSON.parse(this.response)
@@ -107,15 +101,7 @@ function error(err) {
       var data = {
         "results": {
           "sunrise": "1:08:18 PM",
-          "sunset": "3:13:20 AM",
-          "solar_noon": "8:10:49 PM",
-          "day_length": "14:05:02",
-          "civil_twilight_begin": "12:39:02 PM",
-          "civil_twilight_end": "3:42:35 AM",
-          "nautical_twilight_begin": "12:03:12 PM",
-          "nautical_twilight_end": "4:18:25 AM",
-          "astronomical_twilight_begin": "11:24:22 AM",
-          "astronomical_twilight_end": "4:57:15 AM"
+          "sunset": "3:13:20 AM"
         },
         "status": "OK"
       }
@@ -144,6 +130,7 @@ function error(err) {
     console.log('sunset', timeAlter(timeS));
     console.log('sunrise', timeAlter(timeR));
     console.log('actual time', timeA);
+
     let nothingMakesSense = timeAlter(timeS) < timeAlter(timeR); //because of UTC timezone
     let isdaytime = true;
     if ((timeAlter(timeS) < timeA && timeA <= 2400) || (0 <= timeA && timeA < timeAlter(timeR))) {
@@ -152,22 +139,17 @@ function error(err) {
     if (nothingMakesSense) {
       isdaytime = !isdaytime;
     }
-    console.log('final', isdaytime)
+    console.log('daytime:', isdaytime)
 
     var item = document.getElementById("item");
-    console.log(item);
-
     var iImg = document.createElement("img");
     var bgImg = document.getElementById("backgroundimage");
-    console.log('bg', bgImg);
-    console.log('in the error');
-
     if (isdaytime) {
       iImg.setAttribute('src', 'img/balloon.png');
       iImg.setAttribute('alt', 'floating red baloon with long red string attached to it');
       bgImg.style.backgroundImage = 'url("https://raw.githubusercontent.com/Headieh/construction/master/img/clouds.png")';
       bgImg.style.color = "black";
-    } //../img/clouds.png
+    }
     else {
       iImg.setAttribute('src', 'img/astronaut.png');
       iImg.setAttribute('alt', 'floating astronaut');
@@ -177,14 +159,6 @@ function error(err) {
     item.appendChild(iImg);
   }
   request.send()
-
-
 }
 
 navigator.geolocation.getCurrentPosition(success, error, options);
-
-//var position.latitude = 51.5;
-//var position.longitude = -0.1;
-
-//var latitude = 38.6;
-//var longitude = 121.1;
